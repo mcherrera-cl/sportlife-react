e862e67b55f4030150ee65d5ec498cddd6716f06
mcherrera <mcherrera911@gmail.com>
2026-06-25 22:52:16 -0400

    chore: actualizar el nombre base para desplegar en gh-pages condicionado por el modo

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,13 @@ import react from "@vitejs/plugin-react";
 import path from "node:path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: '/sportlife-react/',
+  base: mode === "production" ? "/sportlife-react/" : "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
       "@assets": path.resolve(__dirname, "./src/assets"),
     },
   },
-});
+}));
