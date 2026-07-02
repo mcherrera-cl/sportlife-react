03a27a9d7456a3e88c6afa012e31eef35cde56ec
mcherrera <mcherrera911@gmail.com>
2026-07-02 15:34:26 -0400

    feat: agregar la valicación sobre los password para cuando se crea un usuario: [password 1 debe ser igual a password2]

--- src/utils/validaciones.js
+++ src/utils/validaciones.js
@@ -1,5 +1,5 @@
 export function validarEmail(valor) {
-  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
   const email = String(valor).trim();
   return regex.test(email);
 }
@@ -14,10 +14,24 @@ export function validarNombre(valor) {
   return regex.test(nombre);
 }
 
+export function validarFechaNacimiento(fecha) {
+  const d = new Date(fecha), h = new Date();
+
+  const min = new Date(h.getFullYear() - 100, h.getMonth(), h.getDate());
+  const max = new Date(h.getFullYear() - 10, h.getMonth(), h.getDate());
+  
+  // fecha válida y edad entre 10 y 100 años
+  return !isNaN(d) && d >= min && d <= max;
+}
+
 function validarPassword(input) {
   const esValido = input.value.trim().length >= 8;
 
   input.classList.toggle("is-invalid", !esValido);
 
   return esValido;
+}
+
+export function validarCoincidenciasPassword(pass1, pass2) {
+  return pass1 === pass2;
 }
