d78b8803435bc3190f9e8a565d36fe04ad030c9a
mcherrera <mcherrera911@gmail.com>
2026-07-02 13:51:26 -0400

    feat: funcion que valida el nombre con reglas definidas en la expresión regular

--- src/utils/validaciones.js
+++ src/utils/validaciones.js
@@ -4,6 +4,16 @@ export function validarEmail(valor) {
   return regex.test(email);
 }
 
+export function validarNombre(valor) {
+  const nombre = String(valor)
+    .trim()
+    .replace(/\s+/g, " ");
+
+  const regex = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]{2,}$/;
+
+  return regex.test(nombre);
+}
+
 function validarPassword(input) {
   const esValido = input.value.trim().length >= 8;
 
